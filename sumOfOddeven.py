333e9ff29d06dd040d13bf77ab788c671c476ffb
Rohini <126882312+rohini-1925@users.noreply.github.com>
2023-07-23 20:00:59 +0530

    Update sumOfOddeven.py

--- sumOfOddeven.py
+++ sumOfOddeven.py
@@ -72,3 +72,12 @@ Sample output:
 –21
 Coding:
 ******
+sum1=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+if len(arr)<2:
+    print('-1')
+elif sum(arr[-2:])<=sum1:
+    print(arr[-1]*arr[-2])
+else: 
+    print('0')
